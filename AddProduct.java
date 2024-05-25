9b5c0a53a64d6016c1b91e75683f6b3694787404
denver-iu <111548381+denver-iu@users.noreply.github.com>
2024-05-25 19:47:52 +0800

    Add files via upload

---
+++ AddProduct.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+/**
+ * Servlet implementation class AddProduct
+ */
+@WebServlet("/AddProduct")
+public class AddProduct extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddProduct() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		String productCode = request.getParameter("product_code");
+		pw.printf("\n" + productCode);
+		
+		String productName = request.getParameter("product_name");
+		pw.printf("\n" + productName);
+		String sql;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fproject","root","dbms");
+			Statement stmt = con.createStatement();
+			sql = "INSERT INTO products (ProductCode, ProductName) VALUES ('" + productCode + "','" + productName +"');";
+			stmt.executeUpdate(sql);
+			con.close();
+			
+		} catch (Exception e) {System.out.println(e); }
+		
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		
+	}
+
+}
---
+++ products.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>New Product</title>
+</head>
+<body>
+<form action="AddProduct" method="get">
+	Product Code : <input type="text" name="product_code"><br>
+	Product Name : <input type="text" name="product_name"><br>
+	<input type="Submit"> <input type="Reset">
+</form>
+</body>
+</html>
